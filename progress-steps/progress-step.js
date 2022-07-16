4a713e04d4cc38da742364075c132b2e0eee6aaa
Yusufu1110 <29051099@qq.com>
2022-07-16 15:51:05 +0800

    rotating-nav-animation

--- progress-steps/progress-step.js
+++ progress-steps/progress-step.js
@@ -7,37 +7,38 @@ let lineWidth = 0;
 const tempWidth = 100 / (circles.length - 1);
 
 next.addEventListener("click", () => {
-  if (progress < circles.length - 1) {
-    progress++;
-    updated();
-  }
+    if (progress < circles.length - 1) {
+        progress++;
+        updated();
+    }
 });
 prev.addEventListener("click", () => {
-  if (progress > 0) {
-    progress--;
-    updated();
-  }
+    if (progress > 0) {
+        progress--;
+        updated();
+    }
 });
 circles.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    progress = index;
-    updated();
-  });
+    item.addEventListener("click", () => {
+        progress = index;
+        updated();
+    });
 });
+
 function updated() {
-  fillLine.style.width = `${tempWidth * progress}%`;
-  circles.forEach((item, index) => {
-    if (index <= progress) {
-      item.classList.add("active");
-    } else {
-      item.classList.remove("active");
+    fillLine.style.width = `${tempWidth * progress}%`;
+    circles.forEach((item, index) => {
+        if (index <= progress) {
+            item.classList.add("active");
+        } else {
+            item.classList.remove("active");
+        }
+    });
+    prev.disabled = false;
+    next.disabled = false;
+    if (progress === 0) {
+        prev.disabled = true;
+    } else if (progress === circles.length - 1) {
+        next.disabled = true;
     }
-  });
-  prev.disabled = false;
-  next.disabled = false;
-  if (progress === 0) {
-    prev.disabled = true;
-  } else if (progress === circles.length - 1) {
-    next.disabled = true;
-  }
 }
